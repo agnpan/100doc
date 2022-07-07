21f93ca057bba81df427a91a362b520ff10d55cf
agnpan <agapanster@gmail.com>
2022-07-07 21:16:16 +0200

    Finish Day 6

--- day_006_fp_maze/day_006_fp_maze.py
+++ day_006_fp_maze/day_006_fp_maze.py
@@ -1,4 +1,23 @@
 # Link to the maze:  tinyurl.com/3kkt9zh3
+def move():
+	pass
+
+
+def turn_left():
+	pass
+
+
+def front_is_clear():
+	pass
+
+
+def at_goal():
+	pass
+
+
+def right_is_clear():
+	pass
+
 
 def turn_around():
 	turn_left()
@@ -10,8 +29,9 @@ def turn_right():
 	turn_left()
 
 
-while not is_facing_north():
-	turn_left()
+while front_is_clear():
+	move()
+turn_right()
 
 while not at_goal():
 	if right_is_clear():
