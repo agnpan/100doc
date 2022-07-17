27276ec62c800a88e397e912ecffa1d73e312a58
Agnieszka <agapanster@gmail.com>
2022-07-17 20:38:41 +0200

    Add a functionality to repeat the bidding

--- day_009_fp_silent_auction/fp_silent_auction.py
+++ day_009_fp_silent_auction/fp_silent_auction.py
@@ -35,33 +35,51 @@ def show_highest_bid():
     :return: The name and the value of the highest bid.
     """
     highest_bid = 0
-    winner = []
+    winners = []
     for bid in all_bids.values():
         if bid > highest_bid:
             highest_bid = bid
 
     for person in all_bids.keys():
         if all_bids[person] == highest_bid:
-            winner.append(person)
+            winners.append(person.capitalize())
 
-    if len(winner) == 1:
-        return f"The winner is {winner} with ${highest_bid}."
+    if len(winners) == 1:
+        print(f"The winner is {winners[0]} with ${highest_bid}.")
     else:
-        return f"The winners are {winner} with ${highest_bid}."
+        winners = ", ".join(winners)
+        print(f"The winners are {winners}, with ${highest_bid} each.")
 
 
-print(logo)
-start_again = True
+def setup_new_bidding():
+    """
+    Sets up a new silent auction if the user wants to repeat it.
+    """
+    answer = "answer"
+    while answer[0] not in ("y", "n"):
+        answer = input("Would you like to repeat the auction? Type 'yes' or 'no': ").lower()
+    if answer[0] == "y":
+        silent_auction()
 
-while start_again:
-    take_bid()
-    another_bidder = check_for_other_bidders()
-    if another_bidder:
-        for i in range(51):
-            print()
-    else:
-        start_again = False
 
-print(show_highest_bid())
+def silent_auction():
+    """
+    Enables multiple users to place their bids and reveals the winner.
+    """
+    print(logo)
+    start_again = True
 
-# TODO: figure out what to do when there is more than 1 highest bidder with the same amount
+    while start_again:
+        take_bid()
+        another_bidder = check_for_other_bidders()
+        if another_bidder:
+            for i in range(51):
+                print()
+        else:
+            start_again = False
+
+    show_highest_bid()
+    setup_new_bidding()
+
+
+silent_auction()
