ceea3a9c303ddb4dd7e5cba6b39dd10af9f01c7a
Agnieszka Panczyk/Spanish Capsules (VIR) /SRPOL/Professional/Samsung Electronics <a.panczyk@partner.samsung.com>
2022-07-19 17:27:24 +0200

    Start adding continued operations

--- day_010_fp_calculator/fp_calculator.py
+++ day_010_fp_calculator/fp_calculator.py
@@ -28,11 +28,27 @@ operations = {
 
 
 print(logo)
-num1 = float(input("What's the first number? "))
-for symbol in operations:
-    print(symbol)
-operation_symbol = input("Pick an operator from the line above: ")
-num2 = float(input("What's the second number? "))
 
-calculation = operations[operation_symbol](num1, num2)
-print(f"{num1} {operation_symbol} {num2} = {calculation}")
+extra_operation = True
+
+while extra_operation:
+    num1 = float(input("What's the first number? "))
+    for symbol in operations:
+        print(symbol)
+    operation_symbol = input("Pick an operator from the line above: ")
+    num2 = float(input("What's the second number? "))
+
+    calculation = operations[operation_symbol](num1, num2)
+    print(f"{num1} {operation_symbol} {num2} = {calculation}")
+    
+    continue_calculating = "answer"
+    while continue_calculating[0] not in ("y", "n"):
+        continue_calculating = input("Do you want to add another operation? Type 'yes' or 'no': ").lower()
+    
+    if continue_calculating == "n":
+        break
+    else:
+        pass
+
+
+# TODO Create a while loop to continue asking user whether they want to continue operations
